6da74ff4b5b7398b69e750c174b62d9db58361d4
Gowtham L <gowtham010503@gmail.com>
2023-01-01 18:59:47 +0530

    Update Users.jsx

--- CRUD/Users.jsx
+++ CRUD/Users.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom'
 
 const Users = () => {
 
-  let [data,setData]=useState([])   //We can apply map to array 
+  let [data,setData]=useState([])   
 
-  useEffect(()=>{ //It take two parameter 1 is for callback funtion and 2nd is dependency
-  axios.get("http://localhost:3000/user")   //To get the data
+  useEffect(()=>{ 
+  axios.get("http://localhost:3000/user")   
   .then((response)=>{
     console.log(response.data);
     setData(response.data);   
@@ -16,11 +16,11 @@ const Users = () => {
   .catch((error)=>{
     console.log(error);
   })
-},[]) //If we use empty dependency it will refresh once
+},[]) 
  
 let deletedata=(id)=>{
   axios.delete(`http://localhost:3000/user/${id}`)
-  window.location.assign("/users")      //It is used to refresh the given path's component.
+  window.location.assign("/users")      
 }
 
 return (
@@ -43,6 +43,5 @@ return (
     </div>
   )
 }
-//Higher order data-Function which accepts another order function data
-//Ln.no.34 Using backticks we can concat.To merge two value $ is used.
+
 export default Users
